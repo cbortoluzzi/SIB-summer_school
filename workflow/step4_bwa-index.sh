209cc1905f9c6ae4b14cc8cd4fe0bc409c7b09fa
Chiara Bortoluzzi <54146289+cbortoluzzi@users.noreply.github.com>
2026-06-12 14:39:07 +0200

    Create step4_bwa-index.sh

---
+++ workflow/step4_bwa-index.sh
@@ -0,0 +1,37 @@
+!/bin/bash -l
+
+#!/bin/bash -l
+
+#SBATCH --job-name bwa-index
+#SBATCH --output my_code_%J.out
+#SBATCH --error my_code_%J.err
+#SBATCH --mem 300G
+#SBATCH --partition cpu
+#SBATCH --cpus-per-task 6
+#SBATCH --time 12:00:00
+
+
+
+if [ $# -ne 1 ]
+then
+    	echo -e "\nusage: `basename $0 ` <Reference genome in FASTA format>\n"
+        echo -e "DESCRIPTION: This script generates the index file required for genome alignment in BWA-Mem2\n\n"
+
+        echo -e "INPUT:         <reference genome>       Reference genome in FASTA format (this is the masked reference genome)\n\n"
+
+        echo -e "OUTPUT:        <An indexed reference genome>\n\n"
+
+        echo -e "REQUIRES:      Requires BWA (v0.7.19) available from PATH\n\n"
+
+        exit
+fi
+
+
+reference_genome=$1
+
+echo "Generating index for" $reference_genome
+# Generate index file of reference genome
+/work/PRTNR/SIB/ENVBIO/rwaterho/biomine/summer_school_2026/bin/bwa-mem2/bwa-mem2 index $reference_genome
+
+echo "Done!"
+
